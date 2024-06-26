e948a7ec7c087f0d1fe1caefc5fabf93325fd3d9
Legat48 <daletters01@gmail.com>
2024-06-26 22:47:48 +0300

    fix

--- 4/client/src/components/PostForm/PostForm.tsx
+++ 4/client/src/components/PostForm/PostForm.tsx
@@ -1,5 +1,5 @@
 import { FC, FormEventHandler, useState } from 'react';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '../Button';
 import { FormField } from '../FormField';
 import './PostForm.css';
@@ -8,24 +8,28 @@ import { createPost } from '../../api/Posts';
 export interface IPostFormProps {}
 
 export const PostForm: FC<IPostFormProps> = () => {
-
   const [text, setText] = useState('');
+  const queryClient = useQueryClient();
 
   const createPostMutation = useMutation({
-    mutationFn: () => createPost(text),
-  })
+    mutationFn: (variables: { text: string }) => createPost(variables.text),
+    onSuccess: () => {
+      queryClient.invalidateQueries( { queryKey: ['posts'] });
+    }
+  });
+
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault()
-    createPostMutation.mutate;
+    createPostMutation.mutate({text});
   };
 
   return (
     <form onSubmit={handleSubmit} className="post-form">
       <FormField label="Текст поста">
-        <textarea className="post-form__input" value={text} onChange={(e) => setText(e.currentTarget.value)}/>
+        <textarea className="post-form__input" value={text} onChange={(e) => setText(e.currentTarget.value)} />
       </FormField>
-
-      <Button type="submit" title="Опубликовать" isLoading={createPostMutation.isPending}/>
+      {createPostMutation.error && <span>{(createPostMutation.error as Error).message}</span>}
+      <Button type="submit" title="Опубликовать" isLoading={createPostMutation.isPending} />
     </form>
   );
 };
