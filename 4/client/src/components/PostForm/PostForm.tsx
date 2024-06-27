4f65445d269088e6b5b55a950e47f600b24b8e6a
Legat48 <daletters01@gmail.com>
2024-06-27 13:56:45 +0300

    fix

--- 4/client/src/components/PostForm/PostForm.tsx
+++ 4/client/src/components/PostForm/PostForm.tsx
@@ -1,34 +1,46 @@
-import { FC, FormEventHandler, useState } from 'react';
+import { FC } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '../Button';
 import { FormField } from '../FormField';
 import './PostForm.css';
 import { createPost } from '../../api/Posts';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export interface IPostFormProps {}
 
+const CreatePostSchema = z.object({
+  text: z.string().min(10, 'Текст поста должен быть длиннее 10 символов')
+});
+
+type CreatePostForm = z.infer<typeof CreatePostSchema>;
+
 export const PostForm: FC<IPostFormProps> = () => {
-  const [text, setText] = useState('');
   const queryClient = useQueryClient();
 
-  const createPostMutation = useMutation({
-    mutationFn: (variables: { text: string }) => createPost(variables.text),
+  const { register, handleSubmit, formState: { errors } } = useForm<CreatePostForm>({
+    resolver: zodResolver(CreatePostSchema),
+  });
+
+  const createPostMutation = useMutation<void, unknown, string>({
+    mutationFn: createPost,
     onSuccess: () => {
-      queryClient.invalidateQueries( { queryKey: ['posts'] });
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
     }
   });
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault()
-    createPostMutation.mutate({text});
+  const onSubmit = (data: CreatePostForm) => {
+    createPostMutation.mutate(data.text);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="post-form">
+    <form onSubmit={handleSubmit(onSubmit)} className="post-form">
       <FormField label="Текст поста">
-        <textarea className="post-form__input" value={text} onChange={(e) => setText(e.currentTarget.value)} />
+        <textarea className="post-form__input" {...register('text')} />
+        {errors.text && <span>{errors.text.message}</span>}
       </FormField>
-      {createPostMutation.error && <span>{(createPostMutation.error as Error).message}</span>}
+      {createPostMutation.error instanceof Error && <span>{createPostMutation.error.message}</span>}
       <Button type="submit" title="Опубликовать" isLoading={createPostMutation.isPending} />
     </form>
   );
